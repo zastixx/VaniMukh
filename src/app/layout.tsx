4991ee1c73e6383a4485f38845335cb0dc2e64d1
TARUN KUMAR UTTAM <57785497+zastixx@users.noreply.github.com>
2024-09-30 23:02:12 +0530

    preline_change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from 'next/script';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <script src="./node_modules/preline/dist/preline.js"></script>
+      <Script src="/preline.js" strategy="afterInteractive" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
