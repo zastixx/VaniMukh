179c007613646b551c184d63ecad4a2a09664ac6
TARUN KUMAR UTTAM <57785497+zastixx@users.noreply.github.com>
2024-09-30 23:27:48 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { PrelineScript } from "./components/PrelineScript";
+import Script from 'next/script';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <PrelineScript />
+        <Script src="https://unpkg.com/preline" />
       </body>
     </html>
   );
