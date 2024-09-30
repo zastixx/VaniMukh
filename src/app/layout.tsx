dd388d5c1eb278ca0df9cd82d7abe0615638ae43
TARUN KUMAR UTTAM <57785497+zastixx@users.noreply.github.com>
2024-09-30 23:09:14 +0530

    next_changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { useEffect } from 'react';
 import Script from 'next/script';
 
 const geistSans = localFont({
@@ -21,9 +22,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
+  useEffect(() => {
+    import('preline');
+  }, []);
+
   return (
     <html lang="en">
       <Script src="/preline.js" strategy="afterInteractive" />
